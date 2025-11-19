f23567c23085898d1e18e1df0b1340192e062463
vyachgudzenko <vyach.gudzenko@gmail.com>
2025-11-19 23:00:13 +0200

    init commit

---
+++ UniverseGroupTestTask/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UniverseGroupTestTask
+//
+//  Created by Слава on 19.11.2025.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack{
+            
+        }
+    }
+}
+
+
+
+#Preview {
+    ContentView()
+}
